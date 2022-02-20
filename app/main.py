ecdb2633381543e61a6045015e08d764a86c7a27
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2022-02-20 03:29:36 -0500

    feat: added matrix api to calculate proper time

--- app/main.py
+++ app/main.py
@@ -42,6 +42,15 @@ def getTimeAtCharger(battery_capacity, charging_speed, distance, car_speed):
 
     return net_time
 
+def getTimeAtChargerWithMatrixAPI(battery_capacity, charging_speed, time_to_station):
+    charging_time = battery_capacity/charging_speed
+    print(f"Charging Time: {charging_time} hours")
+
+    net_time = round(charging_time + time_to_station, 5)
+    print(f"Net Time: {net_time} hours")
+
+    return net_time
+
 def distanceToCharger(curr_location, charger_location):
     pass
     # print(res.json)
@@ -57,8 +66,6 @@ def index():
 def findClosestDCFastCharger():
     battery_capacity = request.json["batteryToFill"]  # Amount needed to fill, kWh
     curr_location = request.json["currLocation"] # Address or (lat, long)
-
-    avg_car_speed = request.json["avgCarSpeed"]
 
     if isinstance(curr_location, list):
         req_str = f"https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key={nrel_api_key}&country=all&latitude={curr_location[0]}&longitude={curr_location[1]}&limit=1&ev_charging_level=dc_fast"
@@ -88,9 +95,16 @@ def findClosestDCFastCharger():
                 "pricing": fuelStationDataRaw["ev_pricing"]
             }
 
-    print(fuelStationData)
+    if isinstance(curr_location, list):
+        req_str = f"https://maps.googleapis.com/maps/api/distancematrix/json?origins={curr_location[0]} {curr_location[1]}&destinations={fuelStationData['location']['address']}&key=AIzaSyA-2hrkZS4W_t37X-iYVB4XmeDwwmZC1Dk"
+    else:
+        req_str = f"https://maps.googleapis.com/maps/api/distancematrix/json?origins={curr_location}&destinations={fuelStationData['location']['address']}&key=AIzaSyA-2hrkZS4W_t37X-iYVB4XmeDwwmZC1Dk"
+    
+    response = requests.request("GET", req_str)
+    minsToStation = response.json()["rows"][0]["elements"][0]["duration"]["value"] / 60
 
-    net_time = getTimeAtCharger(battery_capacity, DC_FAST_CHARGER_SPEED, fuelStationData["distance"], avg_car_speed)
+    net_time = getTimeAtChargerWithMatrixAPI(battery_capacity, DC_FAST_CHARGER_SPEED, minsToStation / 60)
+
     return {
         "net_time": net_time,
         "fuel_station": fuelStationData
@@ -100,7 +114,6 @@ def findClosestDCFastCharger():
 def findClosestLevel2Charger():
     battery_capacity = request.json["batteryToFill"]  # Amount needed to fill, kWh
     curr_location = request.json["currLocation"] # Address
-    avg_car_speed = request.json["avgCarSpeed"]
 
     if isinstance(curr_location, list):
         req_str = f"https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key={nrel_api_key}&country=all&latitude={curr_location[0]}&longitude={curr_location[1]}&limit=1&ev_charging_level=2"
@@ -130,9 +143,16 @@ def findClosestLevel2Charger():
                 "pricing": fuelStationDataRaw["ev_pricing"]
             }
 
-    print(fuelStationData)
+    if isinstance(curr_location, list):
+        req_str = f"https://maps.googleapis.com/maps/api/distancematrix/json?origins={curr_location[0]} {curr_location[1]}&destinations={fuelStationData['location']['address']}&key=AIzaSyA-2hrkZS4W_t37X-iYVB4XmeDwwmZC1Dk"
+    else:
+        req_str = f"https://maps.googleapis.com/maps/api/distancematrix/json?origins={curr_location}&destinations={fuelStationData['location']['address']}&key=AIzaSyA-2hrkZS4W_t37X-iYVB4XmeDwwmZC1Dk"
+    
+    response = requests.request("GET", req_str)
+    minsToStation = response.json()["rows"][0]["elements"][0]["duration"]["value"] / 60
 
-    net_time = getTimeAtCharger(battery_capacity, DC_FAST_CHARGER_SPEED, fuelStationData["distance"], avg_car_speed)
+    net_time = getTimeAtChargerWithMatrixAPI(battery_capacity, LEVEL_2_CHARGER_SPEED, minsToStation / 60)
+
     return {
         "net_time": net_time,
         "fuel_station": fuelStationData
@@ -142,7 +162,6 @@ def findClosestLevel2Charger():
 def findClosestSupercharger():
     battery_capacity = request.json["batteryToFill"]  # Amount needed to fill, kWh
     curr_location = request.json["currLocation"] # Address
-    avg_car_speed = request.json["avgCarSpeed"]
 
     if isinstance(curr_location, list):
         req_str = f"https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key={nrel_api_key}&country=all&latitude={curr_location[0]}&longitude={curr_location[1]}&limit=1&ev_network=Tesla"
@@ -172,9 +191,16 @@ def findClosestSupercharger():
                 "pricing": fuelStationDataRaw["ev_pricing"]
             }
 
-    print(fuelStationData)
+    if isinstance(curr_location, list):
+        req_str = f"https://maps.googleapis.com/maps/api/distancematrix/json?origins={curr_location[0]} {curr_location[1]}&destinations={fuelStationData['location']['address']}&key=AIzaSyA-2hrkZS4W_t37X-iYVB4XmeDwwmZC1Dk"
+    else:
+        req_str = f"https://maps.googleapis.com/maps/api/distancematrix/json?origins={curr_location}&destinations={fuelStationData['location']['address']}&key=AIzaSyA-2hrkZS4W_t37X-iYVB4XmeDwwmZC1Dk"
+    
+    response = requests.request("GET", req_str)
+    minsToStation = response.json()["rows"][0]["elements"][0]["duration"]["value"] / 60
 
-    net_time = getTimeAtCharger(battery_capacity, SUPERCHARGER_SPEED, fuelStationData["distance"], avg_car_speed)
+    net_time = getTimeAtChargerWithMatrixAPI(battery_capacity, SUPERCHARGER_SPEED, minsToStation / 60)
+
     return {
         "net_time": net_time,
         "fuel_station": fuelStationData
