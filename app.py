9cfbbb0668c00ac22f8177e61bee3cfe2e7e4905
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2022-02-19 20:16:01 -0500

    feat: first commit

---
+++ app.py
@@ -0,0 +1,175 @@
+from flask import Flask, request
+import requests, json
+from consts import *
+import urllib.request, json
+
+
+app = Flask(__name__)
+
+nrel_api_key = "slTk0Zgzbd1wRRMtssUP8fBWjLHNWHxIhurqB70P"
+
+
+dcFastChargers = [
+    {
+        "name": "",
+        "location": "",
+    }
+]
+
+superChargers = [
+    {
+        "name": "",
+        "location": "",
+    }
+]
+
+location = [
+    {
+        "Starting_point": "",
+    }
+]
+
+
+def getTimeAtCharger(battery_capacity, charging_speed, distance, car_speed):
+    time_to_station = distance/car_speed  # hour
+
+    charging_time = battery_capacity/charging_speed
+    print(f"Charging Time: {charging_time} hours")
+
+    net_time = round(charging_time + time_to_station, 5)
+    print(f"Net Time: {net_time} hours")
+
+    return net_time
+
+def distanceToCharger(curr_location, charger_location):
+    pass
+    # print(res.json)
+  
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'hello'
+
+
+@app.route('/api/findBestDCFastCharger', methods=['POST'])
+def findClosestDCFastCharger():
+    battery_capacity = request.json["batteryToFill"]  # Amount needed to fill, kWh
+    curr_location = request.json["currLocation"] # Address
+    avg_car_speed = request.json["avgCarSpeed"]
+
+    location = '+'.join(''.join([char for char in curr_location if char.isalnum() or char == " "]).split())
+    print(location)
+    req_str = f"https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key={nrel_api_key}&country=all&location={location}&limit=1&ev_charging_level=dc_fast"
+    fuelStationData = {}
+    with requests.get(req_str) as res:
+        resData = res.json()
+        if resData["total_results"] > 0:
+            fuelStationDataRaw = resData["fuel_stations"][0]
+            fuelStationData = {
+                "name": fuelStationDataRaw["station_name"],
+                "phone_number": fuelStationDataRaw["station_phone"],
+                "location": {
+                    "coordinates": (fuelStationDataRaw["latitude"], fuelStationDataRaw["longitude"]),
+                    "address": fuelStationDataRaw["street_address"],
+                    "city": fuelStationDataRaw["city"],
+                    "state": fuelStationDataRaw["state"],
+                    "zip": fuelStationDataRaw["zip"],
+                },
+                "connector_types": fuelStationDataRaw["ev_connector_types"],
+                "ev_network": fuelStationDataRaw["ev_network"],
+                "distance": fuelStationDataRaw["distance_km"],
+                "pricing": fuelStationDataRaw["ev_pricing"]
+            }
+
+    print(fuelStationData)
+
+    net_time = getTimeAtCharger(battery_capacity, DC_FAST_CHARGER_SPEED, fuelStationData["distance"], avg_car_speed)
+    return {
+        "net_time": net_time,
+        "fuel_station": fuelStationData
+    }
+
+@app.route('/api/findBestLevel2Charger', methods=['POST'])
+def findClosestLevel2Charger():
+    battery_capacity = request.json["batteryToFill"]  # Amount needed to fill, kWh
+    curr_location = request.json["currLocation"] # Address
+    avg_car_speed = request.json["avgCarSpeed"]
+
+    location = '+'.join(''.join([char for char in curr_location if char.isalnum() or char == " "]).split())
+    print(location)
+    req_str = f"https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key={nrel_api_key}&country=all&location={location}&limit=1&ev_charging_level=2"
+    fuelStationData = {}
+    with requests.get(req_str) as res:
+        resData = res.json()
+        if resData["total_results"] > 0:
+            fuelStationDataRaw = resData["fuel_stations"][0]
+            fuelStationData = {
+                "name": fuelStationDataRaw["station_name"],
+                "phone_number": fuelStationDataRaw["station_phone"],
+                "location": {
+                    "coordinates": (fuelStationDataRaw["latitude"], fuelStationDataRaw["longitude"]),
+                    "address": fuelStationDataRaw["street_address"],
+                    "city": fuelStationDataRaw["city"],
+                    "state": fuelStationDataRaw["state"],
+                    "zip": fuelStationDataRaw["zip"],
+                },
+                "connector_types": fuelStationDataRaw["ev_connector_types"],
+                "ev_network": fuelStationDataRaw["ev_network"],
+                "distance": fuelStationDataRaw["distance_km"],
+                "pricing": fuelStationDataRaw["ev_pricing"]
+            }
+
+    print(fuelStationData)
+
+    net_time = getTimeAtCharger(battery_capacity, DC_FAST_CHARGER_SPEED, fuelStationData["distance"], avg_car_speed)
+    return {
+        "net_time": net_time,
+        "fuel_station": fuelStationData
+    }
+    
+@app.route('/api/findBestSupercharger', methods=['POST'])
+def findClosestSupercharger():
+    battery_capacity = request.json["batteryToFill"]  # Amount needed to fill, kWh
+    curr_location = request.json["currLocation"] # Address
+    avg_car_speed = request.json["avgCarSpeed"]
+
+    location = '+'.join(''.join([char for char in curr_location if char.isalnum() or char == " "]).split())
+    print(location)
+    req_str = f"https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key={nrel_api_key}&country=all&location={location}&limit=1&ev_network=Tesla"
+    fuelStationData = {}
+    with requests.get(req_str) as res:
+        resData = res.json()
+        if resData["total_results"] > 0:
+            fuelStationDataRaw = resData["fuel_stations"][0]
+            fuelStationData = {
+                "name": fuelStationDataRaw["station_name"],
+                "phone_number": fuelStationDataRaw["station_phone"],
+                "location": {
+                    "coordinates": (fuelStationDataRaw["latitude"], fuelStationDataRaw["longitude"]),
+                    "address": fuelStationDataRaw["street_address"],
+                    "city": fuelStationDataRaw["city"],
+                    "state": fuelStationDataRaw["state"],
+                    "zip": fuelStationDataRaw["zip"],
+                },
+                "connector_types": fuelStationDataRaw["ev_connector_types"],
+                "ev_network": fuelStationDataRaw["ev_network"],
+                "distance": fuelStationDataRaw["distance_km"],
+                "pricing": fuelStationDataRaw["ev_pricing"]
+            }
+
+    print(fuelStationData)
+
+    net_time = getTimeAtCharger(battery_capacity, SUPERCHARGER_SPEED, fuelStationData["distance"], avg_car_speed)
+    return {
+        "net_time": net_time,
+        "fuel_station": fuelStationData
+    }
+
+@app.route('/api/distanceToCharger', methods=['POST'])
+def distanceToChargerEndpoint():
+    # distanceToCharger(curr_location)
+    return ''
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ consts.py
@@ -0,0 +1,5 @@
+# taken from here https://freewiretech.com/difference-between-ev-charging-levels/
+
+SUPERCHARGER_SPEED = 150  # speed of supercharger in kWh
+DC_FAST_CHARGER_SPEED = 70  # speed of dc fast charger in kWh
+LEVEL_2_CHARGER_SPEED = 11 # speed of lvl 2 charger in kWh
